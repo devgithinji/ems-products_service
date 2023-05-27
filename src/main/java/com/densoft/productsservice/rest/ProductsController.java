c77f35a17b018f38ad132738f6d2c763ef99b7ad
dennis githinji <wakahiad@gmail.com>
2023-05-27 19:20:39 +0300

    add axon framework, create new command class, send command to a command gateway

--- src/main/java/com/densoft/productsservice/rest/ProductsController.java
+++ src/main/java/com/densoft/productsservice/rest/ProductsController.java
@@ -1,18 +1,43 @@
 package com.densoft.productsservice.rest;
 
+import com.densoft.productsservice.command.CreateProductCommand;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductsController {
+
+    private final Environment environment;
+    private final CommandGateway commandGateway;
+
+
     @PostMapping
-    public String createProduct() {
-        return "Http post handled";
+    public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .price(createProductRestModel.getPrice())
+                .title(createProductRestModel.getTitle())
+                .quantity(createProductRestModel.getQuantity())
+                .productId(UUID.randomUUID().toString())
+                .build();
+
+        try {
+            return commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception e) {
+            return e.getLocalizedMessage();
+        }
+
+
     }
 
     @GetMapping
     public String getProduct() {
-        return "Http get handled";
+        return "Http get handled: " + environment.getProperty("local.server.port");
     }
 
     @PutMapping
