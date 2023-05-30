8f624c754640f5bed21c162e9df0e2b61f7c6dd7
dennis githinji <wakahiad@gmail.com>
2023-05-30 17:24:42 +0300

    custom error object

--- src/main/java/com/densoft/productsservice/core/errorhandling/ProductsServiceErrorHandler.java
+++ src/main/java/com/densoft/productsservice/core/errorhandling/ProductsServiceErrorHandler.java
@@ -7,17 +7,21 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.Date;
+
 @ControllerAdvice
 public class ProductsServiceErrorHandler {
 
     @ExceptionHandler(value = {IllegalStateException.class})
     public ResponseEntity<Object> handleIllegalStateException(IllegalStateException ex, WebRequest webRequest) {
-        return new ResponseEntity<>(ex.getLocalizedMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleOtherException(Exception ex, WebRequest webRequest) {
-        return new ResponseEntity<>(ex.getLocalizedMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
